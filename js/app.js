9e3aa00788fca5a6af2521a5d818178da2b522e4
Amanda Tennent <amandatennent@me.com>
2016-05-04 22:21:53 +1000

    Updated React script to reuse single modal when displaying phone numbers

--- js/app.js
+++ js/app.js
@@ -5,7 +5,8 @@
         super();
         this.state = {
             searchString: '',
-            contacts: []
+            contacts: [],
+            temp: 1
         };
     }
 
@@ -83,20 +84,44 @@
                         }
                         </tbody>
                     </table>
-                    {
-                        libraries.map(function(item){
-                            if (item.Phone)
-                            {
-                                return(
-                                    <PhoneModal name={item.Name} number={item.Phone} idNumber={item.ID} key={item.ID} />
-                                )
-                            }
-                        })
-                    }
+                    <PhoneModal />
                     <AddContactModal addContact={this._addContact.bind(this)} />
-
                 </section>
         );
+    }
+}
+
+class Contact extends React.Component {
+    render() {
+        let emailBody;
+        let phoneIDHash = "#view-phone" + this.props.idNumber;
+
+        if (this.props.email)
+        {
+            emailBody = "mailto:" + this.props.email;
+        }
+
+        return (
+                <tr>
+                    <td className="v-middle"><a href='#' data-toggle="modal" data-target="#edit-contact">{this.props.name}</a></td>
+                    <td className="v-middle text-right">
+                        {this.props.email && <a href={emailBody}>Email</a>}
+                        {this.props.phone && <a href="#" onClick={this._onClickPhone.bind(this)}>Phone</a>}
+                    </td>
+                </tr>
+        );
+    }
+
+    _onClickPhone()
+    {
+        // Set the header
+        $('#view-phone #view-phone-header').text(this.props.name + "\'s Phone Number");
+
+        // Set the content
+        $('#view-phone #view-phone-content').text(this.props.phone);
+
+        // Show the phone modal
+        $('#view-phone').modal('toggle')
     }
 }
 
@@ -154,17 +179,17 @@ class AddContactModal extends React.Component{
 
 class PhoneModal extends React.Component {
     render() {
-        let elementID = "phoneModal" + this.props.idNumber;
+        let elementID = "view-phone";
         return(
-            <div className="modal fade" id={elementID}>
+            <div className="modal fade" id={elementID} tabindex="-1">
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
                             <button type="button" className="close" data-dismiss="modal">&times;</button>
-                            <h4 className="modal-title">{this.props.name}&#39;s Phone Number</h4>
+                            <h4 id="view-phone-header" className="modal-title">Phone Number</h4>
                         </div>
                         <div className="modal-body">
-                            <p>{this.props.number}</p>
+                            <p id='view-phone-content'>Phone Number</p>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
@@ -172,28 +197,6 @@ class PhoneModal extends React.Component {
                     </div>
                 </div>
             </div>
-        );
-    }
-}
-
-class Contact extends React.Component {
-    render() {
-        let emailBody;
-        let modalIDHash = "#phoneModal" + this.props.idNumber;
-
-        if (this.props.email)
-        {
-            emailBody = "mailto:" + this.props.email;
-        }
-
-        return (
-                <tr>
-                    <td className="v-middle">{this.props.name}</td>
-                    <td className="v-middle text-right">
-                        {this.props.email && <a href={emailBody}>Email</a>}
-                        {this.props.phone && <a href="#" data-toggle="modal" data-target={modalIDHash}>Phone</a>}
-                    </td>
-                </tr>
         );
     }
 }
